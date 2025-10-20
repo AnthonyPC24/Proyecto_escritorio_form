a41ac248e7270d1ff3f08379cc8a1caab09144d8
Anthony Paz Coral <thony1124@gmail.com>
2025-10-20 20:21:13 +0200

    20/10/25

--- FormReunionesDy1.Designer.cs
+++ FormReunionesDy1.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormReunionesDy1";
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            checkBox1 = new CheckBox();
+            domainUpDown1 = new DomainUpDown();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(268, 114);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // domainUpDown1
+            // 
+            domainUpDown1.Location = new Point(268, 139);
+            domainUpDown1.Name = "domainUpDown1";
+            domainUpDown1.Size = new Size(120, 23);
+            domainUpDown1.TabIndex = 3;
+            domainUpDown1.Text = "domainUpDown1";
+            // 
+            // FormReunionesDy1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(domainUpDown1);
+            Controls.Add(checkBox1);
+            Name = "FormReunionesDy1";
+            Text = "FormReunionesDy1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private CheckBox checkBox1;
+        private DomainUpDown domainUpDown1;
     }
 }
